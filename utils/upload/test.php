e10dd8ef67c845e70b6c4adc93907b78c4fde40b
AlexanderSuvorovN <38633357+AlexanderSuvorovN@users.noreply.github.com>
2021-11-22 02:11:38 +0100

    Add files via upload

---
+++ utils/upload/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<link type='text/css' meadia='screen' rel='stylesheet' href='st.css'>
+	<script src='/jquery/jquery-3.5.1.min.js'></script>
+	<script src='test.js'></script>
+</head>
+<body>
+	<input type='file' id='ingredient_file'>
+</body>
+</html>
